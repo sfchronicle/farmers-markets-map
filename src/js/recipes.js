a9e323eadf40393e7680a7124880ffeb9c05eb2c
Emma O'Neill <emmaruthoneill@gmail.com>
2018-02-02 17:54:59 -0800

    re-design

--- src/js/recipes.js
+++ src/js/recipes.js
@@ -71,71 +71,71 @@ $("#searchbar").bind("input propertychange", function () {
 
 });
 
-function executeButton(f,scrollFlag){
-  document.getElementById('recipe-noresults').classList.add("hide");
-
-  if (screen.width <= 480 && scrollFlag) {
-    // animated top scrolling
-    var pos = $("#stick-here").offset().top-37;
-    $('body, html').animate({scrollTop: pos});
-  }
-
-  document.getElementById('searchbar').value = "";
-
-  var classes = f.getAttribute("class");
-  var classes_ls = classes.split(" ");
-
-  // clicked on a filter level filter
-  if (classes.indexOf("subfilter") == -1) {
-    // hide all subfilter containers to start - ADD BACK IN LATER!!!
-    var subfilter_list = document.getElementsByClassName("sub-filter-container");
-    for (var i=0; i<subfilter_list.length; i++) {
-      subfilter_list[i].classList.remove("active");
-    };
-    // check for subfilters that we should display
-    var subfilter_container = document.getElementById("subfilter"+classes_ls[2]);
-    if (subfilter_container) {
-      subfilter_container.classList.add("active");
-    }
-
-    // add active class to chosen filter (if it is a subfilter, we don't need to hide stuff)
-    var filter_list = document.getElementsByClassName("filter");
-    for (var i=0; i<filter_list.length; i++) {
-      filter_list[i].classList.remove("active");
-    };
-    f.classList.add("active");
-
-  // clicked on a subfilter level filter
-  } else {
-    // we just need to add an active class to the subfilter
-    var filter_list = document.getElementsByClassName("subfilter");
-    for (var i=0; i<filter_list.length; i++) {
-      filter_list[i].classList.remove("active");
-    };
-    f.classList.add("active");
-  }
-
-  // show only events that match the chosen filter
-  var recipe_list = document.getElementsByClassName("recipe-item");
-  // no need to check for filter if the person wanted to show everything
-  if (f.id == "showall") {
-    // show all events
-    for (var j=0; j<recipe_list.length; j++) {
-      recipe_list[j].classList.add("active");
-    }
-  // checking each element for the filter
-  } else {
-    // show matching events
-    for (var j=0; j<recipe_list.length; j++) {
-      if (recipe_list[j].classList.toString().indexOf(f.id) >= 0) {
-        recipe_list[j].classList.add("active");
-      } else {
-        recipe_list[j].classList.remove("active");
-      }
-    }
-
-  }
-}
+// function executeButton(f,scrollFlag){
+//   document.getElementById('recipe-noresults').classList.add("hide");
+//
+//   if (screen.width <= 480 && scrollFlag) {
+//     // animated top scrolling
+//     var pos = $("#stick-here").offset().top-37;
+//     $('body, html').animate({scrollTop: pos});
+//   }
+//
+//   document.getElementById('searchbar').value = "";
+//
+//   var classes = f.getAttribute("class");
+//   var classes_ls = classes.split(" ");
+//
+//   // clicked on a filter level filter
+//   if (classes.indexOf("subfilter") == -1) {
+//     // hide all subfilter containers to start - ADD BACK IN LATER!!!
+//     var subfilter_list = document.getElementsByClassName("sub-filter-container");
+//     for (var i=0; i<subfilter_list.length; i++) {
+//       subfilter_list[i].classList.remove("active");
+//     };
+//     // check for subfilters that we should display
+//     var subfilter_container = document.getElementById("subfilter"+classes_ls[2]);
+//     if (subfilter_container) {
+//       subfilter_container.classList.add("active");
+//     }
+//
+//     // add active class to chosen filter (if it is a subfilter, we don't need to hide stuff)
+//     var filter_list = document.getElementsByClassName("filter");
+//     for (var i=0; i<filter_list.length; i++) {
+//       filter_list[i].classList.remove("active");
+//     };
+//     f.classList.add("active");
+//
+//   // clicked on a subfilter level filter
+//   } else {
+//     // we just need to add an active class to the subfilter
+//     var filter_list = document.getElementsByClassName("subfilter");
+//     for (var i=0; i<filter_list.length; i++) {
+//       filter_list[i].classList.remove("active");
+//     };
+//     f.classList.add("active");
+//   }
+//
+//   // show only events that match the chosen filter
+//   var recipe_list = document.getElementsByClassName("recipe-item");
+//   // no need to check for filter if the person wanted to show everything
+//   if (f.id == "showall") {
+//     // show all events
+//     for (var j=0; j<recipe_list.length; j++) {
+//       recipe_list[j].classList.add("active");
+//     }
+//   // checking each element for the filter
+//   } else {
+//     // show matching events
+//     for (var j=0; j<recipe_list.length; j++) {
+//       if (recipe_list[j].classList.toString().indexOf(f.id) >= 0) {
+//         recipe_list[j].classList.add("active");
+//       } else {
+//         recipe_list[j].classList.remove("active");
+//       }
+//     }
+//
+//   }
+// }
 
 // clicking for mobile map interactive
 var qsa = s => Array.prototype.slice.call(document.querySelectorAll(s));
@@ -146,4 +146,4 @@ qsa(".filter").forEach(function(f,index) {
 });
 
 var fallbutton = document.getElementById("fall");
-executeButton(fallbutton,0);
+// executeButton(fallbutton,0);
